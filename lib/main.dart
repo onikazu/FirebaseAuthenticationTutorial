de6cab1a10cce441cd857996ab7ad60156b211cb
onikazu <programingmanagement@gmail.com>
2020-02-09 13:34:00 +0900

    complete tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,12 +19,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final emailInputControler = TextEditingController();
+  final passwordInputControler = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("FirebaseAuthTutorial")),
-      body: Center(child: Text("Initial State")),
+      body: _layoutBody(),
     );
+  }
+
+  Widget _layoutBody() {
+    return Center(
+      child: Form(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(height: 24.0),
+              TextFormField(
+                controller: emailInputControler,
+                decoration: const InputDecoration(
+                  border: const UnderlineInputBorder(),
+                  labelText: "Email"
+                )
+              ),
+              const SizedBox(height: 24.0,),
+              TextFormField(
+                controller: passwordInputControler,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "Password"
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 24.0,),
+              Center(
+                child: RaisedButton(
+                  child: Text("Login"),
+                  onPressed: () {
+                    var email = emailInputControler.text;
+                    // important line
+                    email = email.trim();
+                    var password = passwordInputControler.text;
+                    return _signIn(email, password)
+                      .then((AuthResult result) => print(result.user))
+                      .catchError((e) => print(e));
+                  },
+                ),
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+
+  Future<AuthResult> _signIn(String email, String password) async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    print("User id is ${result.user.uid}");
+    return result;
   }
 }
 
